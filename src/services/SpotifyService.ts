552ca857ee0fc6040bfeb37096d1ac2b223b4109
brunocordioli072 <brunocordioli072@gmail.com>
2020-10-01 15:31:04 -0300

    added console

--- src/services/SpotifyService.ts
+++ src/services/SpotifyService.ts
@@ -3,6 +3,7 @@ import { Service } from "typedi";
 @Service()
 export class SpotifyService extends SpotifyWebApi {
   constructor() {
+    console.log("constructor", process.env.ACCESS_TOKEN);
     super({
       accessToken: process.env.ACCESS_TOKEN ? process.env.ACCESS_TOKEN : "",
       clientId: process.env.CLIENT_ID,
